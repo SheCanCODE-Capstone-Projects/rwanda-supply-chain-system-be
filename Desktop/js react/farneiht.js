9bfa4dcd1858aec36d6a9ea957030b693c4d193b
ESTHER <esterufiteyesu@gmail.com>
2026-07-06 21:01:06 +0200

    firstcommit

--- Desktop/js react/farneiht.js
+++ Desktop/js react/farneiht.js
@@ -4,5 +4,5 @@ function celsiusToFahrenheit(celsius) {
     return (celsius * 9/5) + 32;
 }
 console.log(celsiusToFahrenheit(0));   
-console.log(celsiusToFahrenheit(100));  
-console.log(celsiusToFahrenheit(25));   
+console.log(celsiusToFahrenheit(100 g));  
+console.log(celsiusToFahrenheit(25 ));   
