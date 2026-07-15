42216196b18decb1900d182941a54ad356357a33
rimpuhwe <rimpuhwemanzi@gmail.com>
2026-07-15 09:37:40 +0200

    Feat: Starter code for the entities

---
+++ src/main/java/org/example/rwandasupplychain/Enums/BusinessType.java
@@ -0,0 +1,12 @@
+package org.example.rwandasupplychain.Enums;
+
+public enum BusinessType {
+
+    MANUFACTURER ,
+    WAREHOUSE ,
+    COOPERATIVE ,
+    CONSUMER ,
+    WHOLESALER,
+    RETAILER ,
+    DRIVER,
+}
